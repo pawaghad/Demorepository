9c12c5976ceae8e118b1d9c8a4e2b4c61bd6a1ea
Pavan <pavan.awaghad2@gmail.com>
2022-12-19 10:46:13 +0530

    Basic commit
    HI,
    I have created project go ahead and clone it
    thanks!

---
+++ GithubProject/src/pageLayer/TestBase.java
@@ -0,0 +1,11 @@
+package pageLayer;
+
+
+public class TestBase {
+	public static void main(String args[])
+	{
+		System.out.println("Testbase class executed");
+		System.out.println("Added dependency");
+	}
+
+}
